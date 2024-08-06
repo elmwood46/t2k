634aedcf5f3cb7da5369378459b9b787f47cf890
Ben Read <readbenj@myvuw.ac.nz>
2024-08-06 13:26:36 +1200

    added camera gimbal

---
+++ Util/cameraGimbal.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public partial class cameraGimbal : Node3D
+{
+
+	// Vector3 cameraPos = new Vector3(8, 8, 8);
+	// const float cameraRotation = 45;
+	Camera3D camera;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		camera = GetNode<Camera3D>("Camera3D");
+		
+
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+    public override void _Input(InputEvent @event)
+    {
+        
+		if(@event.IsActionPressed("rotateCameraLeft")){
+			RotateY(-45);
+		}
+
+		if(@event.IsActionPressed("rotateCameraRight")){
+			RotateY(45);
+		}
+    }
+}
