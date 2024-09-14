c7cf6bb573adaffabb676aebef4832c90ce8dd29
Ben Read <readbenj@myvuw.ac.nz>
2024-09-14 12:39:34 +1200

    rename playercharater to navCharacter

--- Charater/NavCharater.cs
+++ Charater/NavCharater.cs
@@ -1,44 +1,36 @@
 using Godot;
 using System;
 
-public partial class PlayerCharacter : CharacterBody3D
+public partial class NavCharater : CharacterBody3D
 {
-public const float Speed = 5.0f;
-	public const float JumpVelocity = 4.5f;
+    [Export]
+    public float Speed = 5.0f;
 
+    [Export]
 	private NavigationAgent3D nav;
-
-	private Vector3 targetPos = new Vector3(0, 1, 0);
-
+	
+    private Vector3 targetPos = new Vector3(0, 1, 0);
 	public Vector3 MovementTarget {
 		get { return nav.TargetPosition; }
 		set { nav.TargetPosition = value;}
 	}
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+	public float gravity = 0.1f;
 
     public override void _Ready()
     {
         base._Ready();
-
-		nav = GetNode<NavigationAgent3D>("%NavigationAgent3D");
 		nav.PathDesiredDistance = 1f;
         nav.TargetDesiredDistance = 0.5f;
-
 		Callable.From(ActorSetup).CallDeferred();
     }
 
     public override void _PhysicsProcess(double delta)
 	{
-
 		if (nav.IsNavigationFinished()) return;
-        
         Vector3 currentAgentPosition = GlobalPosition;
         Vector3 nextPathPosition = nav.GetNextPathPosition();
-
         Velocity = currentAgentPosition.DirectionTo(nextPathPosition) * Speed;
-
         MoveAndSlide();
 	}
 
