c1aa3665f67715fa86e8c08e8529aef64f41e6b0
Ben Read <readbenj@myvuw.ac.nz>
2024-10-21 19:44:32 +1300

    set direction

--- spells/SpellManager.cs
+++ spells/SpellManager.cs
@@ -1,10 +1,13 @@
 using Godot;
 using System;
 
-public partial class SpellManager : Node
+public partial class SpellManager : Node3D
 {
 	[Export]
 	PlayerCharacter player;
+
+	[Export]
+	CameraController cameraController;
 
 	SpellChainComponent spellChainHead;
 
@@ -16,8 +19,31 @@ public partial class SpellManager : Node
 
     public override void _Input(InputEvent @event)
     {
-		if(@event.IsAction("InvokeSpell")){
-			CastPropertys c = new CastPropertys(this, player.Position, new Vector3(1, 0, 0));
+		// if (@event is InputEventMouseButton eventMouseButton && eventMouseButton.Pressed && eventMouseButton.ButtonIndex == MouseButton.Left)
+	    // {
+	        // Vector3 from = GetParent().GetNode<CameraController>("../CameraController").Camera.ProjectRayOrigin(eventMouseButton.Position);
+	        // Vector3 to = from + GetParent().GetNode<CameraController>("../CameraController").Camera.ProjectRayNormal(eventMouseButton.Position) * 4000f;
+			// PhysicsRayQueryParameters3D r = PhysicsRayQueryParameters3D.Create(from, to);
+	    	// Godot.Collections.Dictionary res = GetWorld3D().DirectSpaceState.IntersectRay(r);
+			// Boolean noResult = res.Count <= 0; 
+			// if(noResult) return;
+			// Node node = (Node) res["collider"];
+			// Vector3 p = (Vector3) res["position"];
+
+		// }
+
+
+		if(@event is InputEventMouseButton eventMouseButton && eventMouseButton.Pressed && eventMouseButton.ButtonIndex == MouseButton.Left){
+			Vector3 from = cameraController.Camera.ProjectRayOrigin(eventMouseButton.Position);
+	        Vector3 to = from + cameraController.Camera.ProjectRayNormal(eventMouseButton.Position) * 4000f;
+			PhysicsRayQueryParameters3D r = PhysicsRayQueryParameters3D.Create(from, to);
+	    	Godot.Collections.Dictionary res = GetWorld3D().DirectSpaceState.IntersectRay(r);
+			Boolean noResult = res.Count <= 0; 
+			if(noResult) return;
+			Node node = (Node) res["collider"];
+			Vector3 p = (Vector3) res["position"];
+
+			CastPropertys c = new CastPropertys(this, player.Position, p - player.Position);
 			spellChainHead.Invoke(c);
 		}
     }
